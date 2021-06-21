70596e35385049e16153a7e701d179e9b3cb6212
Tusharrastogi9897 <rastogitushar9897@gmail.com>
2021-06-22 00:58:37 +0530

    Linked List implemented with some conditions & types

---
+++ LinkedList/DoublyLinkedList.cpp
@@ -0,0 +1,166 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    
+    public: 
+        int data;
+        Node * next;
+        Node * prev;
+
+        void traverseLinkedList(){
+            Node* ptr;
+            ptr = this;
+
+            cout<<"\n";
+            while(ptr){
+                cout << ptr -> data << " - > ";
+                ptr = ptr -> next;
+            }
+            cout<<"NULL\n\n";
+
+        }
+
+        void traverseFromEnd(){
+            Node* ptr;
+            ptr = this -> find(this -> sizeOf() - 1);
+
+            cout<<"\n";
+            while(ptr){
+                cout << ptr -> data << " < - ";
+                ptr = ptr -> prev;
+            }
+            cout<<"NULL\n\n";
+
+        }
+
+        int sizeOf(){
+            Node* ptr;
+            ptr = this;
+            int size = 0;
+
+            while(ptr){
+                size += 1;
+                ptr = ptr -> next;
+            }
+            return size;
+        }
+
+        Node* find(int pos){
+            Node* ptr;
+            ptr = this;
+            int size = 0;
+
+            Node* found;
+            found = NULL;
+
+            while(ptr){
+                if(pos == size){
+                    found = ptr;
+                    break;
+                }
+                size += 1;
+                ptr = ptr -> next;
+            }
+
+            return found;
+        }
+
+        void insert(int pos, int data){
+            Node *ptr;
+            Node *new_node;
+
+            new_node = new Node;
+            ptr = this;
+
+            int size = 0;
+            
+            if(ptr -> sizeOf() >= pos)
+                while(ptr){
+                    if(pos == 0){
+                        new_node -> next = ptr -> next;
+                        new_node -> data = ptr -> data;
+                        this -> data = data;
+                        this -> next = new_node;
+                        this -> prev = NULL;
+                        new_node -> prev = this;
+                        break;
+                    }
+                    else if(size == (pos - 1)){
+                        new_node -> data = data;
+                        new_node -> next = ptr -> next;
+                        new_node -> prev = ptr;
+                        ptr -> next = new_node;
+                        break;
+                    }
+                    size += 1;
+                    ptr = ptr -> next;
+                }
+            else
+                cout<<"\nCannot Insert value!!\n";
+        }
+
+        void deleteNode(int pos){
+            Node *ptr;
+            ptr = this;
+
+            int size = 0;
+            
+            if(ptr -> sizeOf() > pos){
+                while(ptr){
+                    if(pos == 0){
+                        ptr -> data = ptr -> next -> data;
+                        ptr -> next = ptr -> next -> next;
+                        ptr -> next -> prev = ptr;
+                        break;
+                    } 
+                    else if((pos - 1) == size){
+                        if(ptr -> next){
+                            ptr -> next = ptr -> next -> next;
+                            ptr -> next -> prev = ptr;
+                        }
+                        else
+                            ptr -> next = NULL;
+                        break;
+                    }
+                    size += 1;
+                    ptr = ptr -> next; 
+                }
+            }
+            else
+                cout<<"\nIndex does not exist!!\n";
+        }
+};
+
+
+
+int main(){
+
+    Node* head;
+    Node* end;
+
+    head = new Node;
+    end = new Node;
+
+    head -> data = 98;
+    head -> next = end;
+    head -> prev = NULL;
+
+    end -> data = 119;
+    end -> prev = head;
+    end -> next = NULL;
+
+    head -> traverseLinkedList();
+    head -> insert(2, 78);
+
+    cout<<"After insertion:";
+    head -> traverseLinkedList();
+
+    head -> deleteNode(1);
+
+    cout<<"After deletion:";
+    head -> traverseLinkedList();
+    head -> traverseFromEnd();
+
+    return 0;
+}
